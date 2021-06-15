ae24c3d99e0b74a38244917c9e3265988bba1d98
mahdixabid <mahdiabid158@gmail.com>
2021-06-15 13:19:50 +0100

    fix header v2

--- sort.h
+++ sort.h
@@ -15,4 +15,11 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void print_list(const listint_t *list);
+void print_revlist(const listint_t *list);
+void print_array(const int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 #endif
