b2b32730f6a8c55014c1e046f8642d33b5d40fe5
mahdixabid <mahdiabid158@gmail.com>
2021-06-15 13:07:48 +0100

    header file

---
+++ sort.h
@@ -0,0 +1,18 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stddef.h>
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+void bubble_sort(int *array, size_t size);
+#endif
